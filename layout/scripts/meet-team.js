c667d9d15980fdbfe4622667a67605b15c444613
Alex Xu <alex.xu.205@gmail.com>
2021-08-19 14:40:49 -0400

    exec description

--- layout/scripts/meet-team.js
+++ layout/scripts/meet-team.js
@@ -7,7 +7,7 @@ const members = [
         img:
             "/images/demo/person.png",
         text:
-            "",
+            "Alex and Marcus manage the overall resources and operations of a company, make major corporate decisions, and act as the main point of contact between the board of directors, employees, and the public.",
     },
     {
         id: 2,
@@ -16,7 +16,7 @@ const members = [
         img:
             "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
         text:
-            "",
+            "Alex and Marcus manage the overall resources and operations of a company, make major corporate decisions, and act as the main point of contact between the board of directors, employees, and the public.",
     },
     {
         id: 3,
@@ -25,7 +25,7 @@ const members = [
         img:
             "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
         text:
-            "",
+            "Thomas Wang is responsible for overseeing the technical infrastructure within the organization, leading a team of IT staff, managing the design and implementation of IT infrastructure, and eliminating security risks.",
     },
     {
         id: 4,
@@ -34,7 +34,34 @@ const members = [
         img:
             "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
         text:
-            "",
+            "Sophia Sun is responsible for the administration of all programs and services and events provided by CORG. This includes the drafting and planning processes and execution process for these services and events, such as webinars, workshops, and Hackathons.",
+    },
+    {
+        id: 5,
+        name: "Sophia Sun",
+        job: "Chief Human Resources Officer",
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        text:
+            "John Doe works internally to build team engagement as well as externally for onboarding new applicants. He/She are constantly brainstorming new ideas and procedures to enhance the CORG experience for all members!",
+    },
+    {
+        id: 6,
+        name: "Sophia Sun",
+        job: "Chief Financial Officer",
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        text:
+            "Bobby Bob serves as one of the main growth leaders of CORG, by coordinating various sponsorship pitches, organizing fundraisers, handling donations and creating innovative ideas as one of the key growth-maker within the organization.",
+    },
+    {
+        id: 7,
+        name: "Sophia Sun",
+        job: "Chief Marketing Officer",
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        text:
+            "Jane Doe oversees all CORG promotional events and activities throughout the term. He/She represents CORG in a positive light, manages the social media presence and devises effective marketing tactics to execute in an optimal manner for success.",
     },
 ];
 // select items
